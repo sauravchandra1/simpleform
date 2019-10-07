646fc74922542665b210ec407414a951fc5dc014
Saurav Chandra <sauravchandra125@gmail.com>
2019-10-07 15:29:43 +0530

    CSS Added

--- app/componenets/Main.js
+++ app/componenets/Main.js
@@ -13,7 +13,7 @@ export default class App extends React.Component {
         return (
             <View style={styles.container}>
                 <View style={styles.header}>
-                    <Text style={styles.heaederText}>- Noter -</Text>
+                    <Text style={styles.headerText}>- Noter -</Text>
                 </View>
                 <ScrollView style={styles.scrollContainer}>
 
@@ -32,6 +32,54 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-
+        flex: 1,
+    },
+    header: {
+        backgroundColor: '#E91E63',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderBottomWidth: 10,
+        borderBottomColor: '#ddd',
+    },
+    headerText: {
+        color: 'white',
+        fontSize: 18,
+        padding: 26,
+    },
+    scrollContainer: {
+        flex: 1,
+        marginBottom: 100,
+    },
+    footer: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        zIndex: 10,
+    },
+    textInput: {
+        alignSelf: 'stretch',
+        color: '#fff',
+        padding: 20,
+        backgroundColor: '#252525',
+        borderTopWidth: 2,
+        borderTopColor: '#ededed',
+    },
+    addButton: {
+        position: 'absolute',
+        zIndex: 11,
+        right: 20,
+        bottom: 90,
+        backgroundColor: '#E91E63',
+        width: 90,
+        height: 90,
+        borderRadius: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        elevation: 8,
+    },
+    addButtonText: {
+        color: '#fff',
+        fontSize: 24,
     },
 });
